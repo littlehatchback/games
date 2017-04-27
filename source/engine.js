460184d250696ca4977cfc85fd212ee35afe86fa
Drew Hurdle <Drew Hurdle>
2017-04-26 21:30:08 -0400

    cool fucking bullshit

--- source/engine.js
+++ source/engine.js
@@ -1,9 +1,48 @@
-var canvas = document.getElementById("gameCanvas")
-var ctx = canvas.getContext("2d")
-window.setInterval(update, 1/60)
 
-function update()
+function LoadGame(filename, callback){
+  var oldGame = document.getElementById('gamescript')
+  if (oldGame != null){
+    oldGame.outerHTML = ""
+    delete oldGame
+  }
+
+  var head = document.getElementsByTagName("head")[0],
+        done = false;
+
+  var newGame = document.createElement('script')
+  newGame.type = 'text/javascript'
+  newGame.src = 'source/' + filename
+  newGame.id = 'gamescript'
+  newGame.onload = newGame.onreadystatechange = function(){
+
+    if ( !done && (!this.readyState ||
+      this.readyState == "loaded" || this.readyState == "complete") ) {
+      done = true;
+      callback(); // execute callback function
+
+      // Prevent memory leaks in IE
+      newGame.onload = newGame.onreadystatechange = null;
+      head.removeChild( newGame );
+    }
+  };
+
+  document.head.appendChild(newGame);
+}
+
+function Setup(){
+  canvas = document.getElementById("gameCanvas")
+  ctx = canvas.getContext("2d")
+  LoadGame('game-template.js', FinishLoadGame)
+}
+function FinishLoadGame(){
+  game = new Game(ctx)
+  window.setInterval(Update, 1/60)
+}
+
+function Update()
 {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	gameloop(ctx)
+  if (game){
+  	ctx.clearRect(0, 0, canvas.width, canvas.height)
+  	game.gameloop()
+  }
 }
--- source/game-template.js
+++ source/game-template.js
@@ -4,13 +4,20 @@ ctx.font = "30px Arial";
 ctx.fillStyle = "red"
 ctx.textAlign = "center"
 
-function gameloop()
-{
-	count++
-	ctx.fillText(shouldFire,100,50);
-	countToFireOn = 500
-	if(count >= countToFireOn)
-	{
-		ctx.fillText("FIRE!!!!", canvas.width/2, canvas.height/2);
-	}
+class Game {
+  constructor(ctx){
+    this.ctx = ctx
+    this.author = 'Drew'
+    this.title = 'Whatever'
+  }
+  gameloop()
+  {
+    count++
+    this.ctx.fillText(shouldFire,100,50);
+    this.countToFireOn = 500
+    if(count >= this.countToFireOn)
+    {
+      this.ctx.fillText("FIRE!!!!", canvas.width/2, canvas.height/2);
+    }
+  }
 }
